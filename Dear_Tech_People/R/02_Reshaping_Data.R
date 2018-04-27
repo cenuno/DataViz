faa8dcc6ccfa2e06e976fa917740d16a0ab5b0d9
cenuno <nuno.e.cristian@gmail.com>
2018-04-26 21:33:37 -0500

    Beginning to reshape data to long format

---
+++ Dear_Tech_People/R/02_Reshaping_Data.R
@@ -0,0 +1,20 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Reshape data to long format 
+# Date:     April 25, 2018
+
+# load necessary packages
+library( tidyverse )
+
+# load necessary data
+df <-
+  url( "https://github.com/cenuno/DataViz/raw/master/Dear_Tech_People/Write_Data/2018-04-25-clean_data.rds" ) %>%
+  gzcon() %>%
+  readRDS()
+
+# transfrom df into a list object
+# and rename it original
+df <-
+  list( original = df )
+
+# end of script #
